ddd2f14afa5a1d1d75e6d1d94727ee69ddbe29b7
abdo-8-mamoon <a01281643799a@gmail.com>
2025-12-19 10:15:49 +0200

    puzzle_8

---
+++ Project/src/problems/puzzle_8.py
@@ -0,0 +1,20 @@
+# Goal state
+GOAL_STATE = (
+    1, 3, 6,
+    5, 0, 2,
+    4, 7, 8
+)
+
+# Example starting state 1
+START_STATE_1 = (
+    1, 2, 3,
+    4, 0, 6,
+    7, 5, 8
+)
+
+# Example starting state 2
+START_STATE_2 = (
+    1, 3, 6,
+    5, 0, 2,
+    4, 7, 8
+)
